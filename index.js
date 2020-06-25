625dbdf5d489919f5e088d0a7181a5f0dacb0fd8
Karl Kanhai <kkanhai2021@mka.org>
2020-06-25 17:12:18 -0400

    fuck my life

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const credentials = {
 	ca: ca
 };
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
   
