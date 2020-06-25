f2ea7653ce1c45ed3bd0b4d4ab76fcd46e93717a
Karl Kanhai <kkanhai2021@mka.org>
2020-06-25 17:38:19 -0400

    damm

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const credentials = {
 };
 */
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
   
