9c8e0687c86a624725df2cb91e8f6fcbb8e818ae
Karl Kanhai <kkanhai2021@mka.org>
2020-07-01 18:02:11 -0400

    damm

--- public/js/tutorboard.js
+++ public/js/tutorboard.js
@@ -214,7 +214,7 @@ var modal = new tingle.modal({
 });
 
 // set content
-modal.setContent("<h1 id='popupTitle'>Welcome to Chesscall</h1><div class='newClass'></div><input style='width:100%; paddding-right: 10%;' type='text' id='codeGoesHere' class='undomove' placeholder='Share this room code with your students'><div></div>");
+modal.setContent("<h1 id='popupTitle'>Welcome to Chesscall</h1><div class='newClass'></div><input style='width:100%; paddding-right: 10%;' type='text' id='codeGoesHere' class='undomove' placeholder='Share this room code with your students' value=''><div></div>");
 
 // add a button
 modal.addFooterBtn('Begin Lesson', 'tingle-btn tingle-btn--primary startBtn', function() {
