18a3cf717d645621789c2f43a41cf56f02cbe6db
Karl Kanhai <kkanhai2021@mka.org>
2020-07-05 19:20:20 -0400

    added a bit more polish

--- public/js/tutorboard.js
+++ public/js/tutorboard.js
@@ -2146,6 +2146,7 @@ modal.addFooterBtn('Copy Code', 'tingle-btn tingle-btn--danger copy', function()
   copyText.select();
   copyText.setSelectionRange(0, 99999); 
   document.execCommand("copy");
+  modal.close();
   
 });
 
