b82439f5fcde2f4d311a9705e8e45669d2198179
Karl Kanhai <kkanhai2021@mka.org>
2020-09-30 16:53:28 -0400

    night owl

--- public/js/groupboard.js
+++ public/js/groupboard.js
@@ -1,5 +1,5 @@
 var room = null;
-var session = null;
+
 
 /*
  * Copyright (c) 2020, Jeff Hlywa (jhlywa@gmail.com)
@@ -1937,13 +1937,7 @@ socket.on("studentcredentials", tokennum => {
   console.log("I received:", tokennum);
   token = tokennum;
   console.log("I made token: ", token);
-  session.connect(token, function(error) {
-    if (error) {
-      console.log("Error connecting: ", error.name, error.message);
-    } else {
-      console.log("Connected to the session.");
-    }
-  });
+  initializeSession();
   
 });
 
@@ -2348,3 +2342,40 @@ socket.on("stopOppTimer", (room) => {
   pauseOppTimer();
   startMyTimer();
 });
+
+
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', function streamCreated(event) {
+    var subscriberOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    };
+    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+  });
+
+  session.on('sessionDisconnected', function sessionDisconnected(event) {
+    console.log('You were disconnected from the session.', event.reason);
+  });
+
+  // initialize the publisher
+  var publisherOptions = {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  };
+  var publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+
+  // Connect to the session
+  session.connect(token, function callback(error) {
+    if (error) {
+      handleError(error);
+    } else {
+      // If the connection is successful, publish the publisher to the session
+      session.publish(publisher, handleError);
+    }
+  });
+}
