17f59b70f7c4ca7cc07b95831a9edd9a50cb2a9f
Karl Kanhai <kkanhai2021@mka.org>
2020-10-02 15:49:29 -0400

    kk

--- public/js/groupboard.js
+++ public/js/groupboard.js
@@ -2380,38 +2380,3 @@ function getName () {
 }
 
 
-function cameraToggle() { 
-  initializeSession()
-  function initializeSession() {
-    var session = OT.initSession(apiKey, sessionId);
-    session.disconnect()
-    var session = OT.initSession(apiKey, sessionId);
-    // Subscribe to a newly created stream
-    session.on('streamCreated', function(event) {
-      session.subscribe(event.stream, 'subscriber', {
-        insertMode: 'append',
-        width: '100%',
-        height: '100%'
-      }, handleError);
-    });
-  
-    // Create a publisher
-    var publisher = OT.initPublisher('publisher', {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%',
-      publishVideo: false
-    }, handleError);
-  
-    // Connect to the session
-    session.connect(token, function(error) {
-      // If the connection is successful, initialize a publisher and publish to the session
-      if (error) {
-        handleError(error);
-      } else {
-        session.publish(publisher, handleError);
-      }
-    });
-  }
-  
-}
